0d40128f8c1a91dbf2d499b06d835c2be0876b15
Prachi <prachishah2401@gmail.com>
2020-06-30 17:17:00 +1000

    Outputing dynamic content using props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
       <h1>Hi, I am a React App</h1>
       <p>This is working now!</p>
-      <Person />
+      <Person name="Prachi" age ="28"/>
+      <Person name="Tej" age ="27">My Hobbies: Racing</Person>
+      <Person name="Niket" age ="24"/>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const person = () => {
-    return <p>I'am a Person!</p>
+const person = (props) => {
+    return (
+        <div>
+            <p>I'am {props.name} and I am {props.age} years old! {props.children}</p>
+        </div>
+    )
 };
 
 export default person;
