40111ca4662a0fa6313880c0a540badebde709be
Prachi <prachishah2401@gmail.com>
2020-06-30 17:30:19 +1000

    Outputing dynamic content using state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I am a React App</h1>
-      <p>This is working now!</p>
-      <Person name="Prachi" age ="28"/>
-      <Person name="Tej" age ="27">My Hobbies: Racing</Person>
-      <Person name="Niket" age ="24"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Prachi', age: 28},
+      {name: 'Tej', age: 27},
+      {name: 'Niket', age: 24}
+    ]
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App</h1>
+        <p>This is working now!</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age ={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age ={this.state.persons[2].age}/>
+      </div>
+    );
+  }
 }
 
 export default App;
