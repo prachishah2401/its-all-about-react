199dde5dba69b3d018ddaff1e754a57799317cd3
Prachi <prachishah2401@gmail.com>
2020-07-15 18:01:10 +1000

    renamed css files by adding .module before .css to make use of css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.module.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -44,20 +44,9 @@ class App extends Component {
   }
 
   render(){
-    const style ={
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    }
 
     let persons = null;
+    let btnClass = '';
 
     if(this.state.showPersons) {
       persons =(
@@ -72,29 +61,26 @@ class App extends Component {
           })}
         </div> 
       );
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color: 'black'
-      //}
+      
+      btnClass = classes.Red;
     } 
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); //classes =['red']
+      assignedClasses.push(classes.red); //classes =['red']
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); //classes =['red', 'bold']
+      assignedClasses.push(classes.bold); //classes =['red', 'bold']
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I am a React App</h1>
-        <p className={classes.join(' ')}>This is working now!</p>
-        <StyleButton alt={this.state.showPersons} onClick={this.togglePeronsHandler}>
+        <p className={assignedClasses.join(' ')}>This is working now!</p>
+        <button className={btnClass} alt={this.state.showPersons} onClick={this.togglePeronsHandler}>
           Toggle Persons
-        </StyleButton>
+        </button>
         {persons}
       </div>
     );
